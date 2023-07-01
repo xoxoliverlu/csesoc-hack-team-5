54171fed14e232055f814e7f1a9a1c1de7664220
Vinayak Fialok <vinayakfialok@Vinayaks-MacBook-Air.local>
2023-07-01 15:41:48 +1000

    adf

--- backend/server.ts
+++ backend/server.ts
@@ -1,14 +1,17 @@
 import express, { Express, Request, Response } from "express";
 import mongoose from "mongoose";
 
-mongoose
-  .connect("mongodb+srv://group5:PuQRWuYgeMalZcNn@memorylane.bjwio1u.mongodb.net/?retryWrites=true&w=majority")
-  .then(() => console.log("connected to mongodb"))
-  .catch((err: any) => console.log(err));
-
 const PORT = 8000;
 const app: Express = express();
 const port = PORT;
+mongoose
+  .connect(
+    "mongodb+srv://group5:PuQRWuYgeMalZcNn@memorylane.bjwio1u.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("connected to mongodb"))
+  .catch((err: any) => console.log(err));
+
+app.post("/")
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
