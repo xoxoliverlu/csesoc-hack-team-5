24c40e13b35ab92b4ff41ecd24da6cfee1c6f64f
Vinayak Fialok <vinayakfialok@Vinayaks-MacBook-Air.local>
2023-07-01 15:29:54 +1000

    mongo

--- backend/server.ts
+++ backend/server.ts
@@ -1,4 +1,15 @@
 import express, { Express, Request, Response } from 'express';
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://group5:<password>@memorylane.bjwio1u.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
 
 const PORT = 8000;
 const app: Express = express();
