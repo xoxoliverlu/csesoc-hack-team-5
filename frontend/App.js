3b693e54f5b8a1b719ec44d44721d63401c59be6
Vinayak Fialok <vinayakfialok@192-168-68-105.tpgi.com.au>
2023-07-01 23:34:22 +1000

    asdasf

--- frontend/App.js
+++ frontend/App.js
@@ -1,31 +1,35 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Home from './views/Home';
-import Tags from './views/Tags';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Home from "./views/Home";
+import Tags from "./views/Tags";
 
 const Stack = createNativeStackNavigator();
 
-export default function App() { 
-  return ( 
-    <NavigationContainer> 
-      <Stack.Navigator> 
-        <Stack.Screen name="Home" component = {Home} /> 
-        <Stack.Screen name="Tags" component = {Tags} /> 
-      </Stack.Navigator> 
-    </NavigationContainer> 
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Tags" component={Tags} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#181922',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#181922",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: "1rem",
+    padding: "2rem",
   },
   paragraph: {
-    color: "#fff"
-  }
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: "2rem",
+  },
 });
--- frontend/views/Home.jsx
+++ frontend/views/Home.jsx
@@ -1,12 +1,53 @@
-import React from "react";
-import { Button, View, Text } from "react-native";
+import React, { useState } from "react";
+import { Button, View, Text, TextInput, StyleSheet, Image } from "react-native";
 import { styles } from "../App";
+import MaterialCommunityIcon from "react-native-vector-icons/MaterialCommunityIcons";
 
 export default function Home({ navigation }) {
+  const [text, setText] = useState("");
+
   return (
     <View style={styles.container}>
-      <Text style={styles.paragraph}>Home Screen</Text>
-      <Button title="Go to next screen" onPress={() => navigation.navigate('Tags')}/>
+      <Text style={homeStyle.title}>SHUFFLE</Text>
+      <Image
+        source={{
+          uri: "https://cdn.discordapp.com/attachments/1124528655784222740/1124662573032284303/cassette.png",
+        }}
+        style = {{ width: 200, height: 200 }}
+      />
+      <Text style={styles.paragraph}>
+        Generate a{" "}
+        <Text style={{ color: "#D471E4" }}>curated travel soundtrack</Text>
+      </Text>
+      <View style={homeStyle.container}>
+        <MaterialCommunityIcon name="magnify" size={30} color="#ffffff" />
+        <TextInput
+          style={homeStyle.searchBox}
+          placeholder="Go to next screen"
+          onChange={setText}
+        />
+      </View>
     </View>
   );
-};
+}
+
+const homeStyle = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    backgroundColor: "#3D3F59",
+    borderRadius: "2rem",
+    padding: "1rem",
+    gap: "0.5rem",
+  },
+  searchBox: {
+    fontSize: "1.5rem",
+    fontWeight: "bold",
+    color: "#676a88",
+  },
+  title: {
+    color: "#D471E4",
+    fontWeight: "bold",
+    fontSize: "4rem",
+  }
+});
