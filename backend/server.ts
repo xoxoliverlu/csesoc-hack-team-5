d1874e6fbefc741f073f33337857d383c9155fdc
Vinayak Fialok <vinayakfialok@Vinayaks-MacBook-Air.local>
2023-07-01 15:40:04 +1000

    adsf

--- backend/server.ts
+++ backend/server.ts
@@ -1,22 +1,17 @@
-import express, { Express, Request, Response } from 'express';
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://group5:<password>@memorylane.bjwio1u.mongodb.net/?retryWrites=true&w=majority";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+import express, { Express, Request, Response } from "express";
+import mongoose from "mongoose";
 
+mongoose
+  .connect("mongodb+srv://group5:PuQRWuYgeMalZcNn@memorylane.bjwio1u.mongodb.net/?retryWrites=true&w=majority")
+  .then(() => console.log("connected to mongodb"))
+  .catch((err: any) => console.log(err));
 
 const PORT = 8000;
 const app: Express = express();
 const port = PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
 });
 
 app.listen(port, () => {
