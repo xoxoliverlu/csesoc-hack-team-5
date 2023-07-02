de56073d8050634e636ab2e5ab26d8e3ede7ff49
Vinayak Fialok <vinayakfialok@Vinayaks-MacBook-Air.local>
2023-07-02 10:35:53 +1000

    ahsgdiuyaejfuyadsftuadsgadsuf

--- frontend/views/Playlist.js
+++ frontend/views/Playlist.js
@@ -1,13 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 import { styles } from "../App";
+import { Audio } from "expo-av";
+import MaterialCommunityIcon from "react-native-vector-icons/MaterialCommunityIcons";
 
 const BE_URL = "http://localhost:3000";
+
+const truncate = (str, n) => {
+  return str.length > n ? str.slice(0, n - 1) + "..." : str;
+};
 
 export default function Playlist({ route, navigator }) {
   const { location, genres } = route.params;
 
   const [tracks, setTracks] = useState([]);
+  const [sound, setSound] = useState();
 
   useEffect(() => {
     console.log(location, genres);
@@ -22,20 +29,50 @@ export default function Playlist({ route, navigator }) {
         body: JSON.stringify({ location: location, genres: genres }),
       })
         .then((res) => res.json())
-        .then((res) => setTracks(res));
+        .then((res) =>
+          setTracks(res.sort(() => 0.5 - Math.random()).slice(0, 5))
+        );
     };
 
     fetchData();
   }, []);
 
-  const Track = ({ imageURL, name, artist }) => {
+  const playTrack = async (url) => {
+    const { sound } = await Audio.Sound.createAsync(url);
+    await sound.playAsync();
+    setSound(sound);
+    await sound.playAsync();
+  };
+
+  useEffect(() => {
+    return sound ? () => sound.unloadAsync() : undefined;
+  }, [sound]);
+
+  const Track = ({ imageURL, name, artist, streamURL, n }) => {
     return (
       <View style={tagStyles.card}>
-        <Image style={{ width: 50, height: 50 }} source={{ uri: imageURL }} />
-        <View style={{ display: "flex", flexDirection: "column" }}>
-          <Text style={tagStyles.tagText}>{name + "\n"}</Text>
-          <Text style={tagStyles.tagText}>{artist}</Text>
+        <View style={tagStyles.card}>
+          <Text style={tagStyles.songText}>{n}</Text>
+          <Image style={{ width: 50, height: 50 }} source={{ uri: imageURL }} />
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+            }}
+          >
+            <Text style={tagStyles.songText}>{truncate(name, 30)}</Text>
+            <Text style={tagStyles.artistText}>{artist}</Text>
+          </View>
         </View>
+        <Pressable onPress={() => playTrack(streamURL)}>
+          <MaterialCommunityIcon
+            name="play"
+            size={30}
+            color="#ffffff"
+            style={{ backgroundColor: "#D471E4", borderRadius: 100 }}
+          />
+        </Pressable>
       </View>
     );
   };
@@ -44,8 +81,14 @@ export default function Playlist({ route, navigator }) {
     <View style={styles.container}>
       <Text style={styles.paragraph}>Your playlist is ready</Text>
       <View style={{ display: "flex" }}>
-        {tracks.map((t) => (
-          <Track imageURL={t.imageURL} name={t.name} artist={t.artist} />
+        {tracks.map((t, i) => (
+          <Track
+            imageURL={t.imageURL}
+            name={t.name}
+            artist={t.artist}
+            streamURL={t.streamURL}
+            n={i + 1}
+          />
         ))}
       </View>
     </View>
@@ -56,16 +99,24 @@ const tagStyles = StyleSheet.create({
   card: {
     display: "flex",
     flexDirection: "row",
+    justifyContent: "space-between",
     flexWrap: "wrap",
     color: "#FFFFFF",
     borderRadius: 10,
     padding: 12,
-    gap: 5,
+    gap: 10,
+    alignItems: "center",
   },
-  tagText: {
+  songText: {
     fontFamily: "Arial",
     fontWeight: "bold",
     fontSize: 20,
     color: "#FFFFFF",
+  },
+  artistText: {
+    fontFamily: "Arial",
+    fontWeight: "bold",
+    fontSize: 17,
+    color: "#DDDDDD",
   },
 });
